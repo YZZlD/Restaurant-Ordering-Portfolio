2beb1875b8ec521ac2fffb25341328b6f45349ec
YZZlD <max.verhulst@hotmail.com>
2025-11-08 19:35:30 -0700

    feat: configure startup and build app set startup configs with npgsql using local system secret keys configure app with boilerplate .net startup.cs code map controllers and build and run app in program.cs

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -1,9 +1,49 @@
 using System;
+using Microsoft.EntityFrameworkCore;
+using RestaurantOrderingSystem.Models;
 
 namespace RestaurantOrderingSystem
 {
     public class Startup
     {
+        private IConfiguration _configuration;
 
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Grabbing connection string from local secret keys
+            string connectionString = _configuration["ConnectionStrings:DefaultConnection"];
+
+            services.AddDbContext<CustomersContext>(options =>
+                options.UseNpgsql(_configuration.GetConnectionString(connectionString)));
+
+            services.AddDbContext<MenuItemsContext>(options =>
+                options.UseNpgsql(_configuration.GetConnectionString(connectionString)));
+
+            services.AddDbContext<CustomersContext>(options =>
+                options.UseNpgsql(_configuration.GetConnectionString(connectionString)));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+        }
     }
 }
