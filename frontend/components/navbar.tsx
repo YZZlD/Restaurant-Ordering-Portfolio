d9cff68069f793f7ae43d07b2e5d8d5da8768ecd
YZZlD <max.verhulst@hotmail.com>
2025-11-11 14:54:50 -0700

    feat: current page emphasis with usePathname from nextjs emphazise what page in the navbar the user is currently on

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,8 +1,16 @@
+'use client'
+
 import { robotoBold, condimentTitle } from "@/app/ui/fonts"
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
+// Navbar to be used for all routes on the website. Consists of the restaurant logo, restaurant name, about/menu/contactus routes, and an
+// emphasized order button linking to the order route.
 export default function Navbar()
 {
+    //We grab the pathname to conditionally modify the appearance of the current landing page.
+    const pathname = usePathname();
+
     return(
             <nav className="my-5 mx-5 px-10 h-1/6 bg-red-700 rounded-md">
                 <div className="flex h-full">
@@ -13,9 +21,9 @@ export default function Navbar()
                         <a href="/" className={`${condimentTitle.className}  restaurantTitle`}>Burger Bites</a>
                     </div>
                     <div className=" w-1/2 flex justify-between px-20 items-center">
-                        <a href="/about" className={`${robotoBold.className} navlink `}>About</a>
-                        <a href="/menu" className={`${robotoBold.className} navlink`}>Menu</a>
-                        <a href="/contact-us" className={`${robotoBold.className} navlink`}>Contact Us</a>
+                        <a href="/about" className={`${robotoBold.className} navlink ${pathname == "/about" ? "currentPage" : ""}`}>About</a>
+                        <a href="/menu" className={`${robotoBold.className} navlink ${pathname == "/menu" ? "currentPage" : ""}`}>Menu</a>
+                        <a href="/contact-us" className={`${robotoBold.className} navlink ${pathname == "/contact-us" ? "currentPage" : ""}`}>Contact Us</a>
                     </div>
                     <div className=" w-1/4 flex justify-center items-center h-full">
                         <div className="border-4 border-black bg-white rounded-md h-1/3 w-1/2 flex justify-center items-center hover:bg-gray-300">
