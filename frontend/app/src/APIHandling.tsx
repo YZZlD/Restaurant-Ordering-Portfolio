2502ecfc0657a7435418cff5795c57c909816db7
YZZlD <max.verhulst@hotmail.com>
2025-11-15 15:09:22 -0700

    rework: localhost formatting

--- frontend/app/src/APIHandling.tsx
+++ frontend/app/src/APIHandling.tsx
@@ -1,6 +1,7 @@
 export async function getMenuItems(): Promise<any[]>{
     try{
-        const res = await fetch(`${process.env.API_URL ? process.env.API_URL : 'http://localhost:5223'}/api/menuItem`);
+        const res = await fetch(`${process.env.API_URL ? process.env.API_URL : 'localhost:5223'}/api/menuItem`);
+        console.log(process.env.API_URL);
         if(!res.ok) throw new Error(`${res.status}`);
 
         const menuItems = await res.json();
@@ -14,7 +15,7 @@ export async function getMenuItems(): Promise<any[]>{
 
 export async function getOrders(): Promise<any[]> {
     try{
-        const res = await fetch('http://localhost:5223/api/order');
+        const res = await fetch(`${process.env.API_URL ? process.env.API_URL : 'localhost:5223'}/api/order`);
         if(!res.ok) throw new Error(`${res.status}`);
 
         const orders = await res.json();
@@ -28,7 +29,7 @@ export async function getOrders(): Promise<any[]> {
 
 export async function postOrder(cart: any[]) : Promise<any>{
     try{
-        const res = await fetch('http://localhost:5223/api/order', {
+        const res = await fetch(`${process.env.API_URL ? process.env.API_URL : 'localhost:5223'}/api/order`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
